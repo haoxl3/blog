eeb0aef80af0ee819de9e0d05ffa1370789baac4
haoxl3 <1098262516@qq.com>
2018-02-18 15:15:44 +0800

    init

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 //加载express模块
 var express = require('express');
+//加载模板
+var swig = require('swig');
 //创建app应用=>NodeJs Http.createServer()
 var app = express();
+
+//设置静态文件托管,当用户访问URL以/public开始，那么将直接返回publick下文件
+app.use('/public',express.static(__dirname+'/public'));
+
+//配置应用模板
+//定义当前应用所使用模板引擎
+//第一个参数：模板引擎的名称，同时也是模板文件的后缀，第二个参数表示用于解析处理模板内容的方法
+app.engine('html', swig.renderFile);
+//设置模板文件存放的目录，第一个参数必须是views,第二个参数是目录
+app.set('views','./views');
+//注册所使用的模板引擎，第一个参数必须是view engine,第二个参数和app.engine这个方法中定义的模板引擎的名称（第一个参数）是一致的
+app.set('view engine','html');
+//在开发过程中，需要取消模板缓存
+swig.setDefaults({cache: false});
 /*
 * 首页
 * */
 app.get('/',function(req,res,next){
-    res.send('<h1>hello world</h1>')
+    //res.send('<h1>hello world</h1>')
+    /*
+    * 读取views目录下的指定文件，解析并返回给客户端
+    * 第一个参数：表示模板的文件，相对于views目录 views/index.html
+    * 第二个参数：传递给模板使用的数据
+    * */
+    res.render('index');
 })
 //监听http请求
 app.listen(8081)
