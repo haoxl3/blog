7830de50f0e8235da86e80c295945cbda32fb861
haoxl3 <1098262516@qq.com>
2018-02-18 23:37:58 +0800

    admin page

--- routers/admin.js
+++ routers/admin.js
@@ -4,7 +4,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/user', function(req,res,next){
-    res.send('admin User');
+router.use(function(req, res, next){
+    if(!req.userInfo.isAdmin){
+        //如果为非管理员
+        res.send('对不起，只有管理员才可进入后台管理');
+        return;
+    }
+    next();
 });
+router.get('/', function(req, res, next){
+    //渲染的界面
+    res.render('admin/index', {
+        userInfo: req.userInfo
+    });
+})
 module.exports = router;
