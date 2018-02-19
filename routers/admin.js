2581f89940d800b3db205f710b197befa3586bb0
haoxl3 <1098262516@qq.com>
2018-02-19 16:08:13 +0800

    分页

--- routers/admin.js
+++ routers/admin.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 
 router.use(function(req, res, next){
     if(!req.userInfo.isAdmin){
@@ -12,10 +13,39 @@ router.use(function(req, res, next){
     }
     next();
 });
+//首页
 router.get('/', function(req, res, next){
     //渲染的界面
     res.render('admin/index', {
         userInfo: req.userInfo
+    });
+});
+//用户管理 
+router.get('/user', function(req, res, next){
+    /**
+     * 从数据库中读取所有的用户,每页显示2条
+     * limit为每页显示条数，skip为跳过几条数据
+     */
+    var page = Number(req.query.page || 1);//要显示的页
+    var limit = 2;
+    var pages = 0;//总页数
+    //.count()可得出数据库中总记录数
+    User.count().then(function(count){
+        //计算总页数
+        pages = Math.ceil(count/limit);
+        //取值不能超过pages
+        page = Math.min(page, pages);
+        //取值不能小于1
+        page = Math.max(page, 1);
+        var skip = (page -1)*limit;
+
+        User.find().limit(limit).skip(skip).then(function(users){
+            res.render('admin/user_index', {
+                userInfo: req.userInfo,
+                users: users,
+                page: page
+            })
+        });
     });
 })
 module.exports = router;
