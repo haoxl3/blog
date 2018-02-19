9edb5dbc3136c056b0048cb2ca50c77c5df650dd
haoxl3 <1098262516@qq.com>
2018-02-19 20:35:54 +0800

    分类的修改

--- routers/admin.js
+++ routers/admin.js
@@ -3,8 +3,9 @@
  */
 var express = require('express');
 var router = express.Router();
+
 var User = require('../models/User');
-var Catetory = require('../models/Category');
+var Category = require('../models/Category');
 
 router.use(function(req, res, next){
     if(!req.userInfo.isAdmin){
@@ -53,22 +54,55 @@ router.get('/user', function(req, res, next){
     });
 });
 
-//分类首页
-router.get('/category', function(req, res, next){
-    res.render('admin/category_index', {
-        userInfo: req.userInfo
-    })
+/*
+* 分类首页
+* */
+router.get('/category', function(req, res) {
+
+    var page = Number(req.query.page || 1);
+    var limit = 10;
+    var pages = 0;
+
+    Category.count().then(function(count) {
+
+        //计算总页数
+        pages = Math.ceil(count / limit);
+        //取值不能超过pages
+        page = Math.min( page, pages );
+        //取值不能小于1
+        page = Math.max( page, 1 );
+
+        var skip = (page - 1) * limit;
+
+        /*
+        * 1: 升序
+        * -1: 降序
+        * */
+        Category.find().sort({_id: -1}).limit(limit).skip(skip).then(function(categories) {
+            res.render('admin/category_index', {
+                userInfo: req.userInfo,
+                categories: categories,
+
+                count: count,
+                pages: pages,
+                limit: limit,
+                page: page
+            });
+        });
+
+    });
+
 });
 
 //添加分类
-router.get('/category/add', function(req, res, next){
+router.get('/category/add', function(req, res){
     res.render('admin/category_add', {
         userInfo: req.userInfo
     });
 });
 
 //分类的保存
-router.post('/category/add', function(req, res, next){
+router.post('/category/add', function(req, res){
     var name = req.body.name || '';
     if(name == ''){
         res.render('admin/error',{
@@ -102,4 +136,80 @@ router.post('/category/add', function(req, res, next){
         })
     });
 });
+
+//分类的修改
+router.get('/category/edit', function(req, res, next){
+    //获取要修改的分类的信息
+    var id = req.query.id || '';
+    //获取要修改的分类信息
+    Category.findOne({
+        _id: id 
+    }).then(function(category){
+        if(!category){
+            res.render('admin/error',{
+                userInfo: req.userInfo,
+                message: '分类信息不存在'
+            });
+        }else{
+            res.render('admin/category_edit',{
+                userInfo: req.userInfo,
+                category: category
+            });
+        }
+    })
+});
+//分类的修改保存
+router.post('/category/edit', function(req, res, next){
+    var id = req.query.id || '';
+    var name = req.body.name || '';
+    //获取要修改的分类信息
+    Category.findOne({
+        _id: id 
+    }).then(function(category){
+        if(!category){
+            res.render('admin/error',{
+                userInfo: req.userInfo,
+                message: '分类信息不存在'
+            });
+            return Promise.reject();
+        }else{
+            //要修改的分类名称是否已经在数据库存在 
+            //当用户没有做任何修改提交时要判断
+            if(name == category.name){
+                res.render('admin/success',{
+                    userInfo: req.userInfo,
+                    message: '修改成功',
+                    url: '/admin/category'
+                });
+                return Promise.reject();
+            }else{
+                //判断修改后的名称是否存在,id不一样，但名称一样
+                return Category.findOne({
+                    _id: {$ne: id},
+                    name: name
+                })
+            }
+        }
+    }).then(function(sameCategory){
+        if(sameCategory){
+            res.render('admin/error',{
+                userInfo: req.userInfo,
+                message: '数据库中已经存在同名分类'
+            }); 
+            return Promise.reject();
+        }else{
+            return Category.update({
+                _id: id
+            },{
+                name: name 
+            })
+        }
+    }).then(function(){
+        res.render('admin/success',{
+            userInfo: req.userInfo,
+            message: '修改成功',
+            url: '/admin/category'
+        });
+    })
+})
 module.exports = router;
