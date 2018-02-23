7b764404c2f7118fcb7c442db617d51792ac14c4
haoxl3 <1098262516@qq.com>
2018-02-23 22:00:34 +0800

    博客详情

--- routers/main.js
+++ routers/main.js
@@ -9,11 +9,13 @@ var Content = require('../models/Content');
 /*
 * 处理通用的数据
 * */
+var data;
 router.use(function (req, res, next) {
     data = {
         userInfo: req.userInfo,
         categories: []
     }
+    //读取顶部的分类
     Category.find().then(function(categories) {
         data.categories = categories;
         next();
@@ -50,4 +52,19 @@ router.get('/', function(req,res,next){
         res.render('main/index', data);
     });
 });
+
+//阅读原文
+router.get('/view', function(req,res,next){
+    var contentId = req.query.contentid || '';
+    Content.findOne({
+        _id: contentId
+    }).then(function(content){
+        data.content = content;
+        //阅读人++
+        content.views++;
+        //保存阅读数
+        content.save();
+        res.render('main/view',data);
+    })
+})
 module.exports = router;
